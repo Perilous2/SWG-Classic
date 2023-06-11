b67b6ab14b70a5254e1ca60f1470f00f6b9b23a3
Perilous2 <frankcrisman@gmail.com>
2023-06-11 05:47:47 -0700

    First commit for SWG-Classic

---
+++ Original bin/conf/config-local.lua
@@ -0,0 +1,6 @@
+ZonesEnabled={"tatooine","naboo","endor","corellia","talus","rori","yavin4","dathomir","dantooine","lok"}
+TrePath="/home/swgemu/git/core3/tre"
+DBName="swgemu"
+DBUser="swgemu"
+DBPass="w*A8RP,FA(??uDW"
+
