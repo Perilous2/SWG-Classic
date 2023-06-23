4ef576ab47c70c3e6932a9edf7c8e9a95c82e71b
Perilous2 <133233825+Perilous2@users.noreply.github.com>
2023-06-23 06:47:38 -0700

    Update config-local.lua

--- Original bin/conf/config-local.lua
+++ Original bin/conf/config-local.lua
@@ -1,6 +1,2 @@
-ZonesEnabled={"tatooine","naboo","endor","corellia","talus","rori","yavin4","dathomir","dantooine","lok"}
-TrePath="/home/swgemu/git/core3/tre"
-DBName="swgemu"
-DBUser="swgemu"
-DBPass="w*A8RP,FA(??uDW"
+
 
